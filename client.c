0e718afe838b294d7efb12981e162629659738ee
Your Name <you@example.com>
2024-12-19 14:58:26 +0000

    garbage problem, transport problem

--- client.c
+++ client.c
@@ -1,7 +1,47 @@
 
+#include <string.h> // forbidden
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+void	send_bit(char bit, pid_t receiver_pid)
+{
+	int res;
+	if (bit == 0)
+		res = kill(receiver_pid, SIGUSR2);
+	else
+		res = kill(receiver_pid, SIGUSR1);
+	// if (res == -1)
+	// 	exit(0);
+}
+
+void	send_message(char *msg, pid_t receiver_pid)
+{
+	int i;
+
+	while (*msg)
+	{
+		i = 0;
+		char c = *msg;
+		while (i < 8)
+		{
+			send_bit(c % 2, receiver_pid);
+			c /= 2;
+			i++;
+		}
+		++msg;
+	}
+	/*
+	i = -1;
+	while (++i < 8)
+		send_bit(0, receiver_pid);
+	*/
+}
 
 int main(int ac, char **av)
 {
-    printf("")
+	if (ac != 3)
+		return (1);
+	pid_t pid = atoi(av[1]);
+	send_message(av[2], pid);
 }
--- server.c
+++ server.c
@@ -1,7 +1,46 @@
 
+#include <stdio.h> // forbidden
+#include <unistd.h>
+#include <signal.h>
 
-
-int main(int ac, char **av)
+int	power_two(int n)
 {
-    
+	int	result;
+	int	i;
+
+	result = 1;
+	i = 0;
+	while (i < n)
+	{
+		result *= 2;
+		i++;
+	}
+	return (result);
+}
+
+void	handler(int sig)
+{
+	static char	result;
+	static int	i;
+
+	if (sig == SIGUSR2)
+		result += power_two(i);
+	i++;
+
+	if (i == 8)
+	{
+		i = 0;
+		write(1, &result, 1);
+		result = 0;
+	}
+}
+
+
+int main()
+{
+	printf("this is the PID : %d\n", getpid());
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+	while (1)
+		pause();
 }
